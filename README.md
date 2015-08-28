29491857ebb65fed74014b61f2357e19edf86c20
Joergen Ibsen <jibz-git@stdip.com>
2015-08-28 17:12:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,188 @@
+
+About
+-----
+
+Most command-line programs have to parse options, so there are a lot of
+different solutions to this problem. Some offer many features, while others
+are more basic.
+
+One of the simpler solutions for C is the [getopt][] function, and its
+extension `getopt_long`. They iterate over the options in `argv`, returning
+them one at a time on successive calls.
+
+One nice thing about them is that they are available on most Unix-like
+operating systems (and usually accompany GCC elsewhere, like Windows).
+Unfortunately, some implementation details vary between platforms.
+
+A potential question is what license the version you get when you include
+them is available under. Some are GPL, others LGPL. There are also ports of
+`getopt` that use more liberal licenses.
+
+`parg` is a parser for `argv` that works similarly to `getopt`, but does not
+aim to be a direct replacement. It attempts to make some choices about how to
+handle the extensions and idiosyncrasies of other `getopt` implementations,
+and document them.
+
+It consists of a single source and include file, written in portable ANSI C.
+It is made available under [CC0][].
+
+[getopt]: https://en.wikipedia.org/wiki/Getopt
+[CC0]: http://creativecommons.org/publicdomain/zero/1.0/
+
+
+Usage
+-----
+
+The include file `parg.h` contains documentation in the form of [doxygen][]
+comments. A configuration file is included, so you can simply run `doxygen`
+to generate documentation in HTML format.
+
+[doxygen]: http://www.doxygen.org/
+
+
+Example
+-------
+
+Here is an example that parses command-line options using `parg_getopt()`:
+
+~~~c
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parg.h"
+
+int main(int argc, char *argv[])
+{
+	struct parg_state ps;
+	int c;
+
+	parg_init(&ps);
+
+	while ((c = parg_getopt(&ps, argc, argv, "hs:v")) != -1) {
+		switch (c) {
+		case 1:
+			printf("nonoption '%s'\n", ps.optarg);
+			break;
+		case 'h':
+			printf("Usage: testparg [-h] [-v] [-s STRING]\n");
+			return EXIT_SUCCESS;
+			break;
+		case 's':
+			printf("option -s with argument '%s'\n", ps.optarg);
+			break;
+		case 'v':
+			printf("testparg 1.0.0\n");
+			return EXIT_SUCCESS;
+			break;
+		case '?':
+			if (ps.optopt == 's') {
+				printf("option -s requires an argument\n");
+			}
+			else {
+				printf("unknown option -%c\n", ps.optopt);
+			}
+			return EXIT_FAILURE;
+			break;
+		default:
+			printf("error: unhandled option -%c\n", c);
+			return EXIT_FAILURE;
+			break;
+		}
+	}
+
+	for (c = ps.optind; c < argc; ++c) {
+		printf("nonoption '%s'\n", argv[c]);
+	}
+
+	return EXIT_SUCCESS;
+}
+~~~
+
+
+Comparison to `getopt`
+----------------------
+
+### Use of global variables
+
+`getopt` uses global variables to store its state between calls. `parg` uses
+a struct `parg_state`, which you must pass with each call.
+
+### Handling of nonoptions
+
+POSIX and BSD `getopt` return `-1` on the first nonoption argument. GNU
+`getopt` by default reorders `argv` (even though it is passed as const), so
+all options come first.
+
+`parg` does not change `argv`, and returns each nonoption as the option
+argument of an option with value `1` (like GNU `getopt`, if `optstring` were
+prefixed by '`-`').
+
+If you wish to process all options first, and have the nonoptions ordered at
+the end of `argv`, you can use `parg_reorder()`:
+
+~~~c
+	optend = parg_reorder(argc, argv, optstring, NULL);
+
+	while ((c = parg_getopt(&ps, optend, argv, optstring)) != -1) {
+		/* ... */
+	}
+
+	/* elements of argv[] from optend to argc are nonoptions */
+~~~
+
+### Value of `optind` on error
+
+When there are multiple short options in one argument, `getopt` does not
+increment `optind` until the last one is processed. This makes it harder to
+tell which argument an unknown option came from (if `a` is an unknown option,
+`-a` and `-ab` will return '`?`' with different values in `optind`).
+
+`parg` always increments the `optind` value in it's state so it points to the
+next `argv` element to be processed. So when `parg` returns '`?`' (or '`:`'),
+the element that contains the error is `argv[optind - 1]`.
+
+### Value of `optopt` on error
+
+With `getopt_long`, it varies what the values of `optopt` and `longindex` are
+when an error is found with option arguments of long options. Sometimes these
+values are not documented.
+
+`parg` sets `optopt` to `val` if `flag` is `NULL`, and `0` otherwise (which
+equals the return value on successful match), and `longindex` is set to the
+index of the entry in `longopts` that matched.
+
+### Return value on option argument error
+
+When the first character of `optstring` is '`:`', it varies what `getopt`
+returns on extraneous option arguments.
+
+In this case, `parg` returns '`?`' if no option match is found, and '`:`' if
+a match is found, but is missing a required argument, or has an extraneous
+argument.
+
+
+Alternatives
+------------
+
+Some ports of `getopt`:
+
+  - [Free Getopt](http://freegetopt.sourceforge.net/)
+  - [ya_getopt](http://github.com/kubo/ya_getopt/)
+  - [getopt_port](http://github.com/kimgr/getopt_port/)
+
+Other command-line parsing libraries that support C:
+
+  - [Gengetopt](http://www.gnu.org/software/gengetopt/)
+  - [Argp](http://www.gnu.org/software/libc/manual/html_node/Argp.html)
+  - [popt](http://en.wikipedia.org/wiki/Popt)
+  - [argtable](http://argtable.sourceforge.net/)
+  - [optlist](http://michael.dipperstein.com/optlist/)
+  - [Arg_parser](http://www.nongnu.org/arg-parser/arg_parser.html)
+  - [Gopt](http://www.purposeful.co.uk/software/gopt/)
+  - [docopt](http://docopt.org/)
+
+A few C++ command-line parsing libraries:
+
+  - [TCLAP](http://tclap.sourceforge.net/)
+  - [program_options](http://www.boost.org/doc/libs/1_58_0/doc/html/program_options.html)
+  - [CommandLine](http://llvm.org/docs/CommandLine.html)
---
+++ test/test_parg.c
@@ -0,0 +1,658 @@
+/*
+ * test_parg - parg unit test
+ *
+ * Written in 2015 by Joergen Ibsen
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
+ * public domain worldwide. This software is distributed without any
+ * warranty. <http://creativecommons.org/publicdomain/zero/1.0/>
+ */
+
+#include "parg.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "greatest.h"
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+const char *os_def = ":no::r:";
+
+const struct parg_option po_zero[] = { { 0 } };
+
+const struct parg_option po_def[] = {
+	{ "noarg", PARG_NOARG, NULL, 'n' },
+	{ "optarg", PARG_OPTARG, NULL, 'o' },
+	{ "reqarg", PARG_REQARG, NULL, 'r' },
+	{ "foo", PARG_NOARG, NULL, 'f' },
+	{ "foobar", PARG_NOARG, NULL, 'b' },
+	{ 0 }
+};
+
+/*
+ * parg_getopt tests
+ */
+
+TEST parg_getopt_app_only(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(1, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_dash(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ(1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(argv[1], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_double_dash(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_nonopt(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "foo" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ(1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(argv[1], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_no_match(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-u" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ('?', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ('u', ps.optopt);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_noarg(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-n" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ('n', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_optarg_missing(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-o" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ('o', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_optarg_inline(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-oarg" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ('o', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(&argv[1][2], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_reqarg_missing(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-r" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ(':', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ('r', ps.optopt);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_reqarg_inline(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-rarg" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ('r', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(&argv[1][2], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_reqarg_nextarg(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-r", "arg" };
+
+	parg_init(&ps);
+
+	ASSERT_EQ('r', parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(argv[2], ps.optarg);
+	ASSERT_EQ(3, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt(&ps, ARRAY_SIZE(argv), argv, os_def));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(3, ps.optind);
+
+	PASS();
+}
+
+/*
+ * parg_getopt_long tests
+ */
+
+TEST parg_getopt_long_app_only(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(1, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_dash(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "-" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ(1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(argv[1], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_double_dash(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_nonopt(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "foo" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ(1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(argv[1], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_no_match(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--unknown" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('?', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_flag(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--flag" };
+	int li = -1;
+	int flag = 0;
+	struct parg_option po_flag[] = {
+		{ "flag", PARG_NOARG, &flag, 1 },
+		{ 0 }
+	};
+
+	parg_init(&ps);
+
+	ASSERT_EQ(0, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_flag, &li));
+	ASSERT_EQ(0, li);
+	ASSERT_EQ(1, flag);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_flag, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_noarg(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--noarg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('n', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(0, li);
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_noarg_inline(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--noarg=arg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ(':', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(0, li);
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ('n', ps.optopt);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_optarg_missing(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--optarg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('o', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(1, li);
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_optarg_inline(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--optarg=arg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('o', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(1, li);
+	ASSERT_EQ(&argv[1][9], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_reqarg_missing(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--reqarg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ(':', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(2, li);
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ('r', ps.optopt);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_reqarg_inline(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--reqarg=arg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('r', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(2, li);
+	ASSERT_EQ(&argv[1][9], ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_reqarg_nextarg(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--reqarg", "arg" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('r', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(2, li);
+	ASSERT_EQ(argv[2], ps.optarg);
+	ASSERT_EQ(3, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(3, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_prefix_ambiguous(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--fo" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('?', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_prefix_exact(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--foo" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('f', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(3, li);
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+TEST parg_getopt_long_prefix_unambiguous(void)
+{
+	struct parg_state ps;
+	char *argv[] = { "app", "--foob" };
+	int li = -1;
+
+	parg_init(&ps);
+
+	ASSERT_EQ('b', parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(4, li);
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	ASSERT_EQ(-1, parg_getopt_long(&ps, ARRAY_SIZE(argv), argv, ":", po_def, &li));
+	ASSERT_EQ(NULL, ps.optarg);
+	ASSERT_EQ(2, ps.optind);
+
+	PASS();
+}
+
+/*
+ * parg_reorder tests
+ */
+
+TEST parg_reorder_short(void)
+{
+	char *argv[] = {
+		"app", "first", "-r", "--", "foo", "-u", "bar", "-o", "--",
+		"-n", "last"
+	};
+	char *argv_expected[] = {
+		"app", "-r", "--", "-u", "-o", "--", "first", "foo", "bar",
+		"-n", "last"
+	};
+	size_t i;
+
+	ASSERT_EQ(6, parg_reorder(ARRAY_SIZE(argv), argv, os_def, NULL));
+
+	for (i = 0; i < ARRAY_SIZE(argv); ++i) {
+		ASSERT_STR_EQ(argv_expected[i], argv[i]);
+	}
+
+	PASS();
+}
+
+TEST parg_reorder_long(void)
+{
+	char *argv[] = {
+		"app", "first", "--reqarg", "--", "foo", "--unknown", "bar",
+		"--optarg", "--", "--noarg", "last"
+	};
+	char *argv_expected[] = {
+		"app", "--reqarg", "--", "--unknown", "--optarg", "--",
+		"first", "foo", "bar", "--noarg", "last"
+	};
+	size_t i;
+
+	ASSERT_EQ(6, parg_reorder(ARRAY_SIZE(argv), argv, "", po_def));
+
+	for (i = 0; i < ARRAY_SIZE(argv); ++i) {
+		ASSERT_STR_EQ(argv_expected[i], argv[i]);
+	}
+
+	PASS();
+}
+
+SUITE(parg_getopt_tests)
+{
+	RUN_TEST(parg_getopt_app_only);
+
+	RUN_TEST(parg_getopt_dash);
+
+	RUN_TEST(parg_getopt_double_dash);
+
+	RUN_TEST(parg_getopt_nonopt);
+
+	RUN_TEST(parg_getopt_no_match);
+
+	RUN_TEST(parg_getopt_noarg);
+
+	RUN_TEST(parg_getopt_optarg_missing);
+	RUN_TEST(parg_getopt_optarg_inline);
+
+	RUN_TEST(parg_getopt_reqarg_missing);
+	RUN_TEST(parg_getopt_reqarg_inline);
+	RUN_TEST(parg_getopt_reqarg_nextarg);
+}
+
+SUITE(parg_getopt_long_tests)
+{
+	RUN_TEST(parg_getopt_long_app_only);
+
+	RUN_TEST(parg_getopt_long_dash);
+
+	RUN_TEST(parg_getopt_long_double_dash);
+
+	RUN_TEST(parg_getopt_long_nonopt);
+
+	RUN_TEST(parg_getopt_long_no_match);
+
+	RUN_TEST(parg_getopt_long_flag);
+
+	RUN_TEST(parg_getopt_long_noarg);
+	RUN_TEST(parg_getopt_long_noarg_inline);
+
+	RUN_TEST(parg_getopt_long_optarg_missing);
+	RUN_TEST(parg_getopt_long_optarg_inline);
+
+	RUN_TEST(parg_getopt_long_reqarg_missing);
+	RUN_TEST(parg_getopt_long_reqarg_inline);
+	RUN_TEST(parg_getopt_long_reqarg_nextarg);
+
+	RUN_TEST(parg_getopt_long_prefix_ambiguous);
+	RUN_TEST(parg_getopt_long_prefix_exact);
+	RUN_TEST(parg_getopt_long_prefix_unambiguous);
+}
+
+SUITE(parg_reorder_tests)
+{
+	RUN_TEST(parg_reorder_short);
+	RUN_TEST(parg_reorder_long);
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char *argv[])
+{
+	GREATEST_MAIN_BEGIN();
+	RUN_SUITE(parg_getopt_tests);
+	RUN_SUITE(parg_getopt_long_tests);
+	RUN_SUITE(parg_reorder_tests);
+	GREATEST_MAIN_END();
+}
